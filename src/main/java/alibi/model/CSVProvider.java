4f4fd95eff507b678661ff8db1c7bd2942ab7d4d
adrianhollow <tphan@hm.edu>
2018-12-06 23:14:02 +0100

    changed TimeFilter

--- src/main/java/alibi/model/CSVProvider.java
+++ src/main/java/alibi/model/CSVProvider.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 
 import java.util.Collection;
 import java.util.TreeSet;
+import java.util.logging.Logger;
 
 /**
  * Class for providing alibis from a CSV file.
--- src/main/java/alibi/model/TimeFilter.java
+++ src/main/java/alibi/model/TimeFilter.java
@@ -1,11 +1,11 @@
 package alibi.model;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Class which filters alibis according to the timespan.
+ *
  * @author tphan
  * @version 1.0.0
  * 19.11.2018
@@ -15,7 +15,7 @@ public final class TimeFilter extends AlibiFilter {
     /**
      * The default format expected of a date.
      */
-    public static final String DEFAULT_DATE_FORMAT = "HH.mm dd.MM.yyyy";
+    public static final String DEFAULT_DATE_FORMAT = "HH.mm dd.MM.YYYY";
 
     /**
      * The start date of the filter.
@@ -28,54 +28,41 @@ public final class TimeFilter extends AlibiFilter {
     private final String end;
 
     /**
-     * The date format of the filter.
+     * Format for parsing time input.
      */
-    private final String format;
+    private DateTimeFormatter format;
 
     /**
      * Constructor.
-     * @param newStart  start.
-     * @param newEnd    end.
+     * @param newStart start.
+     * @param newEnd   end.
      */
     public TimeFilter(final String newStart, final String newEnd) {
         this.start = newStart;
         this.end = newEnd;
-        this.format = DEFAULT_DATE_FORMAT;
-    }
-
-    /**
-     * Constructor.
-     * @param newStart  start.
-     * @param newEnd    end.
-     * @param newFormat    format.
-     */
-    public TimeFilter(final String newStart,
-                      final String newEnd, final String newFormat) {
-        this.start = newStart;
-        this.end = newEnd;
-        this.format = newFormat;
+        this.format = DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT);
     }
 
     @Override
     public boolean filter(final Alibi alibi) {
-        return (compare(alibi.getStart(), start) <= 0 && compare(start, alibi.getEnd()) < 0) || (compare(end, alibi.getEnd()) <= 0 && compare(alibi.getStart(), end) < 0);
+        final boolean frontExcess = compare(alibi.getStart(), start) <= 0;
+        final boolean frontOverlap = compare(start, alibi.getEnd()) <= 0;
+        final boolean backExcess = compare(end, alibi.getEnd()) <= 0;
+        final boolean backOverlap = compare(alibi.getStart(), end) <= 0;
+        return (frontExcess && frontOverlap) || (backExcess && backOverlap);
     }
 
     /**
      * Compares two dates in String format with a given format.
-     * @param firstDate   the first date.
-     * @param secondDate  the second date.
+     * @param firstDate  the first date.
+     * @param secondDate the second date.
      * @return -1, if firstDate is before secondDate.
-     *         0, if firstDate is equal to secondDate.
-     *         1, if firstDate is after secondDate.
+     * 0, if firstDate is equal to secondDate.
+     * 1, if firstDate is after secondDate.
      */
     public int compare(final String firstDate, final String secondDate) {
-        try {
-            Date first = new SimpleDateFormat(format).parse(firstDate);
-            Date second = new SimpleDateFormat(format).parse(secondDate);
-            return first.compareTo(second);
-        } catch (ParseException pex) {
-            throw new RuntimeException(pex.getMessage());
-        }
+        LocalDateTime first = LocalDateTime.parse(firstDate, format);
+        LocalDateTime second = LocalDateTime.parse(secondDate, format);
+        return first.compareTo(second);
     }
 }
